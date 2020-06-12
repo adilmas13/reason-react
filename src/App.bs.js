fe884737ed5139166d12c38fd8d2f14ad284e136
Adil Shaikh <adilmas13@gmail.com>
2020-06-12 18:36:31 +0530

    added state and effect

--- src/App.bs.js
+++ src/App.bs.js
@@ -1,13 +1,48 @@
 'use strict';
 
+var Curry = require("bs-platform/lib/js/curry.js");
 var React = require("react");
-var InnerComponent$ReasonReactExamples = require("./InnerComponent/InnerComponent.bs.js");
+var InnerComponent$ReasonReactExamples = require("./InnerComponent.bs.js");
 
 function App(Props) {
-  var name = Props.name;
-  return React.createElement("div", undefined, React.createElement("button", undefined, "Hello " + (name + "!")), React.createElement(InnerComponent$ReasonReactExamples.make, {
-                  children: React.createElement("div", undefined, "This is a child component")
+  Props.name;
+  var match = React.useState((function () {
+          return 0;
+        }));
+  var setCount = match[1];
+  var count = match[0];
+  var match$1 = React.useState((function () {
+          return 0;
+        }));
+  var setAnotherCount = match$1[1];
+  var anotherCount = match$1[0];
+  var incrementCounter = function (param) {
+    return Curry._1(setCount, (function (prevCount) {
+                  return prevCount + 1 | 0;
                 }));
+  };
+  var decrementCounter = function (param) {
+    return Curry._1(setAnotherCount, (function (prevCount) {
+                  return prevCount + 1 | 0;
+                }));
+  };
+  React.useEffect((function () {
+          console.log("EFFECT one");
+          return (function (param) {
+                    console.log("Something");
+                    
+                  });
+        }), /* tuple */[
+        count,
+        anotherCount
+      ]);
+  return React.createElement("div", undefined, React.createElement("button", {
+                  onClick: incrementCounter
+                }, "Increament"), React.createElement("button", {
+                  onClick: decrementCounter
+                }, "Decrement"), React.createElement("button", undefined, "Clicked"), React.createElement(InnerComponent$ReasonReactExamples.make, {
+                  children: null
+                }, React.createElement("div", undefined, "This is a child component"), React.createElement("div", undefined, "Count => " + String(count)), React.createElement("div", undefined, "Another Count => " + String(anotherCount))));
 }
 
 var make = App;
--- src/App.re
+++ src/App.re
@@ -1,13 +1,39 @@
 open ReasonReact;
 
-  [@react.component]
-  let make = (~name) => {
-    <div> <button> {string("Hello " ++ name ++ "!")} </button> 
-    <InnerComponent>
-    <div>
-      {string("This is a child component")}
-    </div>
-    </InnerComponent>
+[@react.component]
+let make = (~name) => {
 
-    </div>;
-  };
+
+let (count, setCount) = React.useState(() => 0);
+
+let (anotherCount, setAnotherCount) = React.useState(() => 0);
+
+let incrementCounter = (_) => setCount(prevCount => prevCount + 1);
+
+let decrementCounter = (_) => setAnotherCount(prevCount => prevCount + 1);
+
+let test = (_, isIncrement:bool) => {
+  if(count >= 10){
+    ();
+  }else{
+  setCount(prevCount => isIncrement ? prevCount + 1 : prevCount - 1)
+  }
+}
+
+React.useEffect2(() =>{
+  Js.log("EFFECT one");
+  Some(() => Js.log("Something"));
+}, (count, anotherCount));
+
+
+  <div>
+    <button onClick=incrementCounter> {string("Increament")} </button>
+     <button onClick=decrementCounter> {string("Decrement")} </button>
+     <button>{string("Clicked")}</button>
+    <InnerComponent>
+      <div> {string("This is a child component")} </div>
+      <div>{string("Count => " ++ string_of_int(count) )}</div>
+            <div>{string("Another Count => " ++ string_of_int(anotherCount) )}</div>
+    </InnerComponent>
+  </div>
+};
