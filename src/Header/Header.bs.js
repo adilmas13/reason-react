64fe1b58a0c6f6e8cc7fadb32bffa13f80d0ed92
Adil Shaikh <adilmas13@gmail.com>
2020-06-13 00:11:58 +0530

    basic navigation via header

--- src/Header/Header.bs.js
+++ src/Header/Header.bs.js
@@ -1,16 +1,47 @@
 'use strict';
 
 var React = require("react");
+var Belt_Array = require("bs-platform/lib/js/belt_Array.js");
+var ReasonReactRouter = require("reason-react/src/ReasonReactRouter.bs.js");
 
 function Header(Props) {
+  var links = [
+    {
+      title: "Home",
+      href: "/"
+    },
+    {
+      title: "Users",
+      href: "/users"
+    }
+  ];
   var headerStyle = {
     backgroundColor: "#2e86c1",
+    display: "flex",
     height: "70px",
     width: "100vw"
   };
+  var navItemStyle = {
+    color: "#fff",
+    cursor: "pointer",
+    display: "flex",
+    fontWeight: "500",
+    height: "100%",
+    width: "100px",
+    alignItems: "center",
+    justifyContent: "center"
+  };
   return React.createElement("div", {
               style: headerStyle
-            });
+            }, Belt_Array.map(links, (function (item) {
+                    return React.createElement("div", {
+                                key: item.title,
+                                style: navItemStyle,
+                                onClick: (function (__x) {
+                                    return ReasonReactRouter.push(item.href);
+                                  })
+                              }, item.title);
+                  })));
 }
 
 var make = Header;
--- src/Header/Header.re
+++ src/Header/Header.re
@@ -1,6 +1,36 @@
+open ReasonReact;
+open ReactDOMRe.Style;
+type navigation = {title:string, href:string};
+
 [@react.component]
 let make = () => {
-    let headerStyle = ReactDOMRe.Style.make(~height ="70px", ~width="100vw", ~backgroundColor="#2e86c1", ());
+    let links = [|{
+        title:"Home",
+        href : "/"
+    },
+    {
+        title:"Users",
+        href : "/users"
+    }|]
 
-    <div style=(headerStyle)></div>
+
+    let headerStyle = make(~height ="70px", ~width="100vw", ~backgroundColor="#2e86c1", ~display="flex", ());
+
+    let navItemStyle = make(~color="#fff", ~fontWeight="500", ~cursor= "pointer", ~height = "100%", ~width="100px", ~display="flex", ~alignItems =  "center", ~justifyContent="center", ());
+
+    let redirect = (_,navigation: navigation) => ReasonReactRouter.push(navigation.href);
+
+    <div style=(headerStyle)>
+     {
+      links
+      ->Belt.Array.map(item =>
+          <div 
+          onClick=redirect(_,item)
+          style=(navItemStyle) key={item.title}> {React.string(item.title)} </div>
+        )
+      /* Since everything is typed, the arrays need to be, too! */
+      ->React.array
+    }
+
+    </div>
 }
